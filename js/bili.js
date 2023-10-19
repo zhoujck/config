9e269c8bc6cbc713b2cf1b40b90cbb3d59f411dd
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-10-19 16:44:35 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -259,7 +259,7 @@ async function category(tid, page, filter, ext) {
             //let cd = getFullTime(item.duration);
 
             video.vod_remarks = list.index_show;
-            video.vod_id ='ss' + list.media_id;
+            video.vod_id = list.media_id;
             video.vod_name = removeTags(list.title);
             video.vod_pic = pic;
 
@@ -285,10 +285,10 @@ async function category(tid, page, filter, ext) {
 
 async function detail(ids) {
     try {
-        const bvid = ids;
-        const detailUrl = `https://api.bilibili.com/pgc/review/user?media_id=${bvid}`;
+        const media_id = ids;
+        const detailUrl = `https://api.bilibili.com/pgc/review/user?media_id=${media_id}`;
 
-        const detailData = JSON.parse(await request(detailUrl, getHeaders())).result;
+        const detailData = JSON.parse(await request(detailUrl, getHeaders())).media;
         // 记录历史
     /*    if (!_.isEmpty(bili_jct)) {
             const historyReport = 'https://api.bilibili.com/x/v2/history/report';
@@ -305,14 +305,14 @@ async function detail(ids) {
         const video = {
             vod_id: media_id,
             vod_name: detailData.title,
-            vod_pic: detailData.pic,
+            vod_pic: detailData.cover,
             type_name: detailData.type_name,
             vod_year: '',
             vod_area: '',
            // vod_remarks: detailData.score,
             vod_actor: '',
             vod_director: '',
-            vod_content: detailData.desc,
+           // vod_content: detailData.desc,
         };
 
         const playurldata = 'https://api.bilibili.com/x/player/playurl?avid=' + aid + '&cid=' + detailData.cid + '&qn=127&fnval=4048&fourk=1';
