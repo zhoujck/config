c3ed2966291a99d49d5b8d9772836670735f5228
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-10-19 13:49:42 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -197,6 +197,7 @@ async function category(tid, page, filter, ext) {
             tid = ext['tid'];
         }
         let url = '';
+/*       
         url = `https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&type=1&season_type=3&st=3style_id={tid}`;
 
         if (Object.keys(ext).length > 0) {
@@ -208,15 +209,18 @@ async function category(tid, page, filter, ext) {
             }
            
         }
-
+*/
         url += `&page=${encodeURIComponent(page)}`;
 
    /* class_name:'历史&人文&宇宙&自然&动物&科技&美食&探险',
     class_url:'10033&10065&10068&10072&10071&10066&10045&10067',*/
 
-        if (tid == "首页") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=-1&type=1&season_type=3&st=3" + page;
+        if (tid == "历史") {
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10033&type=1&season_type=3&st=3" + page;
         } 
+          if (tid == "首页") {
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=-1&type=1&season_type=3&st=3" + page;
+        }     
         else if (tid == "历史记录") {
             url = "https://api.bilibili.com/x/v2/history?pn=" + page;
         }
