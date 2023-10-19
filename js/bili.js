5186736c66b6fdcb679a366a087379daeadc7677
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-10-19 13:11:56 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -198,15 +198,15 @@ async function category(tid, page, filter, ext) {
         }
         let url = '';
         url = `https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=-1&type=1&season_type=3&st=3`;
-/*
+
         if (Object.keys(ext).length > 0) {
             for (const k in ext) {
                 if (k == 'tid') {
                     continue;
                 }
-                url += `&${encodeURIComponent(k)}=${encodeURIComponent(ext[k])}`;
+               // url += `&${encodeURIComponent(k)}=${encodeURIComponent(ext[k])}`;
             }
-*/            
+           
         }
 
         url += `&page=${encodeURIComponent(page)}`;
