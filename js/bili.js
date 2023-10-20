5b0c3835ee5b0b78e337c795d3350fefe7424236
zhoujck <114135342+zhoujck@users.noreply.github.com>
2023-10-20 10:52:56 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -237,33 +237,34 @@ async function category(tid, page, filter, ext) {
     class_url:'10033&10065&10068&10072&10071&10066&10045&10067',*/
 
         if (tid == "首页") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=-1&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=-1&type=1&season_type=3&st=3&page="+page;
         }     
         else if (tid == "历史记录") {
             url = "https://api.bilibili.com/x/v2/history?pn=" + page;
         }
        else if (tid == "历史") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10033&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?pagesize=20&style_id=10033&type=1&season_type=3&st=3&page="+page;
         }
+/*           
          else if (tid == "人文") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10065&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10065&type=1&season_type=3&st=3&page="+page;
         }
          else if (tid == "宇宙") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10068&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10068&type=1&season_type=3&st=3&page="+page;
         }
          else if (tid == "自然") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10072&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10072&type=1&season_type=3&st=3&page="+page;
         }
          else if (tid == "动物") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10071&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10071&type=1&season_type=3&st=3&page="+page;
         }
          else if (tid == "科技") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10066&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10066&type=1&season_type=3&st=3&page="+page;
         }
          else if (tid == "探险") {
-            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10067&type=1&season_type=3&st=3&pagesize=20&page="+page;
+            url = "https://api.bilibili.com/pgc/season/index/result?order=2&pagesize=20&style_id=10067&type=1&season_type=3&st=3&page="+page;
         }
-   
+*/   
         
         const data = JSON.parse(await request(url, getHeaders())).data;
         let items = data.result;
