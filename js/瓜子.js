e9604e0d0330e11ba4adea4cd2221896a28fefe1
zhoujck <114135342+zhoujck@users.noreply.github.com>
2026-05-14 13:39:53 +0800

    Update 瓜子.js

--- js/瓜子.js
+++ js/瓜子.js
@@ -25,7 +25,7 @@ var KParams = {
 };
 
 const CLASSES = [
-    { type_id: "1", type_name: "电影" },
+    { type_id: "1", type_name: "电影1" },
     { type_id: "2", type_name: "电视剧" },
     { type_id: "4", type_name: "动漫" },
     { type_id: "3", type_name: "综艺" },
