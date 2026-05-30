121c0b19cb779f3ca23175868d41a51eb280f052
zhoujck <114135342+zhoujck@users.noreply.github.com>
2026-05-30 15:01:45 +0800

    Create bili.js

---
+++ js/bili.js
@@ -0,0 +1,154 @@
+let host = 'https://api.bilibili.com';
+let headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+    "Referer": "https://www.bilibili.com",
+    "Cookie": ""
+};
+
+async function init(cfg) {}
+
+function getVideos(json) {
+    let videos = [];
+    if (json["code"] !== 0) return videos;
+    let vodList = json.data && json.data.result ? json.data.result : [];
+    vodList.forEach(vod => {
+        let aid = (vod["bvid"] || vod["aid"] || "").toString().trim();
+        let title = vod["title"].trim().replace(/<em class="keyword">/g, "").replace(/<\/em>/g, "");
+        let img = vod["pic"] ? vod["pic"].trim() : "";
+        if (img.indexOf("//") === 0) img = "https:" + img;
+        let remark = vod["duration"] || "";
+        videos.push({
+            vod_id: aid,
+            vod_name: title,
+            vod_pic: img,
+            vod_remarks: remark
+        });
+    });
+    return videos;
+}
+
+async function home(filter) {
+    return JSON.stringify({
+        "class": [
+            {"type_id": "all", "type_name": "全部象棋"},
+            {"type_id": "0", "type_name": "板鸭象棋"},
+            {"type_id": "1", "type_name": "四郎讲棋"},
+            {"type_id": "2", "type_name": "小植象棋"}
+        ],
+        "filters": {
+            "all": [{"key": "order", "name": "排序", "value": [{"n": "最多播放", "v": "click"}, {"n": "最新发布", "v": "pubdate"}, {"n": "最多弹幕", "v": "dm"}, {"n": "最多收藏", "v": "stow"}]}],
+            "0": [{"key": "order", "name": "排序", "value": [{"n": "最多播放", "v": "click"}, {"n": "最新发布", "v": "pubdate"}, {"n": "最多弹幕", "v": "dm"}, {"n": "最多收藏", "v": "stow"}]}],
+            "1": [{"key": "order", "name": "排序", "value": [{"n": "最多播放", "v": "click"}, {"n": "最新发布", "v": "pubdate"}, {"n": "最多弹幕", "v": "dm"}, {"n": "最多收藏", "v": "stow"}]}],
+            "2": [{"key": "order", "name": "排序", "value": [{"n": "最多播放", "v": "click"}, {"n": "最新发布", "v": "pubdate"}, {"n": "最多弹幕", "v": "dm"}, {"n": "最多收藏", "v": "stow"}]}]
+        }
+    });
+}
+
+async function homeVod() {
+    let url = host + "/x/web-interface/search/type?keyword=" + encodeURIComponent("象棋") + "&search_type=video&page=1&order=click";
+    let resp = await req(url, {headers: headers});
+    let jo = JSON.parse(resp.content);
+    return JSON.stringify({list: getVideos(jo)});
+}
+
+async function category(tid, pg, filter, extend) {
+    let p = pg || 1;
+    let order = (extend && extend.order) ? extend.order : "click";
+    let keyword = "";
+    if (tid === "all") {
+        keyword = "象棋";
+    } else if (tid === "0") {
+        keyword = "板鸭象棋";
+    } else if (tid === "1") {
+        keyword = "四郎讲棋";
+    } else if (tid === "2") {
+        keyword = "小植象棋";
+    } else {
+        keyword = "象棋";
+    }
+    let url = host + "/x/web-interface/search/type?keyword=" + encodeURIComponent(keyword) + "&search_type=video&page=" + p + "&order=" + order;
+    let resp = await req(url, {headers: headers});
+    let jo = JSON.parse(resp.content);
+    return JSON.stringify({
+        "list": getVideos(jo),
+        "page": parseInt(p)
+    });
+}
+
+async function detail(id) {
+    let url = host + "/x/web-interface/view?bvid=" + id;
+    let resp = await req(url, {headers: headers});
+    let jo = JSON.parse(resp.content);
+    if (jo.code !== 0) {
+        return JSON.stringify({list: []});
+    }
+    let data = jo.data;
+    let stat = data.stat || {};
+    let status = "播放: " + stat.view + "　弹幕: " + stat.danmaku + "　点赞: " + stat.like + "　投币: " + stat.coin;
+    let pages = data.pages || [];
+    let playurls = [];
+    pages.forEach(pg => {
+        let part = pg.part || ("P" + pg.page);
+        playurls.push(part + "$" + pg.cid);
+    });
+    return JSON.stringify({
+        list: [{
+            vod_id: id,
+            vod_name: data.title,
+            vod_pic: data.pic,
+            type_name: "象棋",
+            vod_year: "",
+            vod_area: "大陆",
+            vod_remarks: data.owner ? data.owner.name : "",
+            vod_actor: status,
+            vod_director: "",
+            vod_content: data.desc || "",
+            vod_play_from: "B站",
+            vod_play_url: playurls.join("#")
+        }]
+    });
+}
+
+async function search(wd, quick, pg) {
+    let p = pg || 1;
+    let url = host + "/x/web-interface/search/type?keyword=" + encodeURIComponent(wd) + "&search_type=video&page=" + p;
+    let resp = await req(url, {headers: headers});
+    let jo = JSON.parse(resp.content);
+    return JSON.stringify({list: getVideos(jo)});
+}
+
+async function play(flag, id, flags) {
+    let cid = id;
+    if (!/^\d+$/.test(id)) {
+        let url = host + "/x/web-interface/view?bvid=" + id;
+        let resp = await req(url, {headers: headers});
+        let jo = JSON.parse(resp.content);
+        if (jo.code === 0 && jo.data) {
+            cid = jo.data.pages && jo.data.pages.length > 0 ? jo.data.pages[0].cid : jo.data.cid;
+        }
+    }
+    let url = host + "/x/player/playurl?bvid=" + id + "&cid=" + cid + "&qn=80&fnval=16";
+    let resp = await req(url, {headers: headers});
+    let jo = JSON.parse(resp.content);
+    if (jo.code === 0 && jo.data) {
+        if (jo.data.dash) {
+            let videoList = jo.data.dash.video || [];
+            let videoUrl = videoList.length > 0 ? (videoList[0].baseUrl || videoList[0].base_url) : "";
+            return JSON.stringify({
+                parse: 0,
+                url: videoUrl,
+                header: {"Referer": "https://www.bilibili.com", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"}
+            });
+        }
+        if (jo.data.durl && jo.data.durl.length > 0) {
+            return JSON.stringify({
+                parse: 0,
+                url: jo.data.durl[0].url,
+                header: {"Referer": "https://www.bilibili.com", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"}
+            });
+        }
+    }
+    return JSON.stringify({parse: 1, url: "https://www.bilibili.com/video/" + id, header: {"User-Agent": "Mozilla/5.0"}});
+}
+
+export default {init, home, homeVod, category, detail, search, play};
