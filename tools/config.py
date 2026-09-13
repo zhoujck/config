c95a372bddd5b85fe93bbd9e812a12e36c683f9d
zhoujck <114135342+zhoujck@users.noreply.github.com>
2026-09-13 08:42:40 +0800

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -17,8 +17,8 @@ SOURCES = [
     {
         "name": "xiaomi",
         #"url": "https://www.tangsan.fun/tv/",
-        "url": "https://zzzy.shop/tv/",
-        # "url": "https://raw.githubusercontent.com/ggrrttyyiii/CatVodSpider/refs/heads/main/json/demo.json",
+        #"url": "https://zzzy.shop/tv/",
+        "url": "https://raw.githubusercontent.com/ggrrttyyiii/CatVodSpider/refs/heads/main/json/demo.json",
     },
 ]
 
